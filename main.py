862f28fd06efc65b95ed4597dc5ec1594c94d0f9
AhmetFurkann <35719856+AhmetFurkann@users.noreply.github.com>
2021-01-19 23:14:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 from kivymd.uix.button import MDFillRoundFlatButton
 from kivy.uix.screenmanager import ScreenManager
-from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.textfield import MDTextField
+from kivymd.uix.boxlayout import MDBoxLayout
+from operations import GetRowColScreen
 from kivymd.uix.screen import Screen
 from kivymd.app import MDApp
 
 
 class GetInfoScreen(Screen):
-    def __init__(self, gamer_number):
+    def __init__(self):
         super(GetInfoScreen, self).__init__()
 
         self.cols_info_input = MDTextField(hint_text="Number Of Cols")
         self.cols_info_input.size_hint_x = .5
         self.cols_info_input.pos_hint = {"center_x": .5, "center_y": .6}
-
         self.rows_info_input = MDTextField(hint_text="Number Of Rows")
         self.rows_info_input.size_hint_x = .5
         self.rows_info_input.pos_hint = {"center_x": .5, "center_y": .5}
 
-        self.create_table_button = MDFillRoundFlatButton(text="Create Table For Player " + gamer_number)
+        self.create_table_button = MDFillRoundFlatButton(text="Create Table For Player")
         self.create_table_button.size_hint_x = .5
         self.create_table_button.pos_hint = {"center_x": .5, "center_y": .4}
 
@@ -32,22 +32,37 @@ class GetInfoScreenManager(Screen):
         super(GetInfoScreenManager, self).__init__()
         main_layout = MDBoxLayout()
 
-        first_player_screen_manager = ScreenManager()
-        first_player_info_screen = GetInfoScreen("1")
-        first_player_info_screen.name = "first_player_info"
-        first_player_screen_manager.add_widget(first_player_info_screen)
-        first_player_screen_manager.current = "first_player_info"
+        self.first_player_screen_manager = ScreenManager()
+        self.first_player_info_screen = GetInfoScreen()
+        self.first_player_info_screen.name = "first_player_info"
+        self.first_player_info_screen.create_table_button.bind(on_press=self.button_change_player_1_screen)
+        self.first_player_screen_manager.add_widget(self.first_player_info_screen)
+        self.first_player_screen_manager.current = "first_player_info"
 
-        second_player_screen_manager = ScreenManager()
-        second_player_info_screen = GetInfoScreen("2")
-        second_player_info_screen.name = "second_player_info"
-        second_player_screen_manager.add_widget(second_player_info_screen)
-        second_player_screen_manager.current = "second_player_info"
+        self.second_player_screen_manager = ScreenManager()
+        self.second_player_info_screen = GetInfoScreen()
+        self.second_player_info_screen.name = "second_player_info"
+        self.second_player_screen_manager.add_widget(self.second_player_info_screen)
+        self.second_player_screen_manager.current = "second_player_info"
 
-        main_layout.add_widget(first_player_screen_manager)
-        main_layout.add_widget(second_player_screen_manager)
+        main_layout.add_widget(self.first_player_screen_manager)
+        main_layout.add_widget(self.second_player_screen_manager)
 
         self.add_widget(main_layout)
+
+    def add_player_1_input_screen(self):
+        rows_number = int(self.first_player_info_screen.rows_info_input.text)
+        cols_number = int(self.first_player_info_screen.cols_info_input.text)
+        get_rows_cols_screen = GetRowColScreen(rows_number, cols_number)
+        get_rows_cols_screen.name = "first_player_input"
+        self.first_player_screen_manager.add_widget(get_rows_cols_screen)
+
+    def change_player_1_input_screen(self):
+        self.add_player_1_input_screen()
+        self.first_player_screen_manager.current = "first_player_input"
+
+    def button_change_player_1_screen(self, instance):
+        self.change_player_1_input_screen()
 
 
 class MainScreenManager(ScreenManager):
@@ -55,15 +70,19 @@ class MainScreenManager(ScreenManager):
         super(MainScreenManager, self).__init__()
         self.home_screen = GetInfoScreenManager()
         self.home_screen.name = "home_screen"
-        
-        # Todo: Add A Screen For the Tables
+
+
         self.table_screen = Screen()
-        self.table_screen.name = "another_screen"
-        
+        self.table_screen.name = "table_screen"
+
         self.add_widget(self.home_screen)
         self.add_widget(self.table_screen)
-        
+
         self.current = "home_screen"
+
+
+def change_screen(self, *args):
+    self.current = "home_screen"
 
 
 class GameTheoryApp(MDApp):
